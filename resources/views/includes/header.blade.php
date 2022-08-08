d6925078e2dbee7df53014ee0bcd5e12dc1f6a46
Oleg <good.my.max@gmail.com>
2022-08-08 08:01:04 +0300

    add multilang

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -2,7 +2,7 @@
 <header id="head">
     <div class="container">
         <div class="row">
-            <h1 class="lead">AWESOME, CUSTOMIZABLE, FREE</h1>
+            <h1 class="lead">{{__('front.slide_text')}}</h1>
             <p class="tagline">PROGRESSUS: free business bootstrap template by <a href="http://www.gettemplate.com/?utm_source=progressus&amp;utm_medium=template&amp;utm_campaign=progressus">GetTemplate</a></p>
             <p><a class="btn btn-default btn-lg" role="button">MORE INFO</a> <a class="btn btn-action btn-lg" role="button">DOWNLOAD NOW</a></p>
         </div>
